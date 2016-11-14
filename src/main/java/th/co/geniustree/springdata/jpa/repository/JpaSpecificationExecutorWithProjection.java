3ebacc28db5f72e33cc37db299dc1c780f6655dd
pramoth <pramoth@geniustree.co.th>
2016-11-14 15:36:27 +0700

    support entity graph

--- src/main/java/th/co/geniustree/springdata/jpa/repository/JpaSpecificationExecutorWithProjection.java
+++ src/main/java/th/co/geniustree/springdata/jpa/repository/JpaSpecificationExecutorWithProjection.java
@@ -3,6 +3,8 @@ package th.co.geniustree.springdata.jpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.query.JpaEntityGraph;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
@@ -11,4 +13,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface JpaSpecificationExecutorWithProjection<T> {
   public <R> Page<R> findAll(Specification<T> spec,Class<R> projectionClass, Pageable pageable);
+  public <R> Page<R> findAll(Specification<T> spec, Class<R> projectionType, String namedEntityGraph, EntityGraph.EntityGraphType type, Pageable pageable);
+  public <R> Page<R> findAll(Specification<T> spec, Class<R> projectionClass, JpaEntityGraph dynamicEntityGraph, Pageable pageable);
 }
--- src/main/java/th/co/geniustree/springdata/jpa/repository/support/JpaSpecificationExecutorWithProjectionImpl.java
+++ src/main/java/th/co/geniustree/springdata/jpa/repository/support/JpaSpecificationExecutorWithProjectionImpl.java
@@ -1,35 +1,80 @@
 package th.co.geniustree.springdata.jpa.repository.support;
 
-import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.query.Jpa21Utils;
+import org.springframework.data.jpa.repository.query.JpaEntityGraph;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
 
+import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by pramoth on 9/29/2016 AD.
  */
 public class JpaSpecificationExecutorWithProjectionImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements JpaSpecificationExecutorWithProjection<T> {
+    private static final Logger log = LoggerFactory.getLogger(JpaSpecificationExecutorWithProjectionImpl.class);
     private final EntityManager entityManager;
     private final ProjectionFactory projectionFactory = new SpelAwareProxyProjectionFactory();
+    private final JpaEntityInformation entityInformation;
 
     public JpaSpecificationExecutorWithProjectionImpl(JpaEntityInformation entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
         this.entityManager = entityManager;
+        this.entityInformation = entityInformation;
     }
+
 
     @Override
     public <R> Page<R> findAll(Specification<T> spec, Class<R> projectionType, Pageable pageable) {
         TypedQuery<T> query = getQuery(spec, pageable);
+        return readPageWithProjection(spec, projectionType, pageable, query);
+    }
+
+    @Override
+    public <R> Page<R> findAll(Specification<T> spec, Class<R> projectionType, String namedEntityGraph, org.springframework.data.jpa.repository.EntityGraph.EntityGraphType type, Pageable pageable) {
+        EntityGraph<?> entityGraph = this.entityManager.getEntityGraph(namedEntityGraph);
+        if (entityGraph == null) {
+            throw new IllegalArgumentException("Not found named entity graph -> " + namedEntityGraph);
+        }
+        TypedQuery<T> query = getQuery(spec, pageable);
+        query.setHint(type.getKey(), entityGraph);
+        return readPageWithProjection(spec, projectionType, pageable, query);
+    }
+
+    @Override
+    public <R> Page<R> findAll(Specification<T> spec, Class<R> projectionType, JpaEntityGraph dynamicEntityGraph, Pageable pageable) {
+        TypedQuery<T> query = getQuery(spec, pageable);
+        Map<String, Object> entityGraphHints = new HashMap<String, Object>();
+        entityGraphHints.putAll(Jpa21Utils.tryGetFetchGraphHints(this.entityManager, dynamicEntityGraph, getDomainClass()));
+        applyEntityGraphQueryHints(query, entityGraphHints);
+        return readPageWithProjection(spec, projectionType, pageable, query);
+    }
+
+    private <R> Page<R> readPageWithProjection(Specification<T> spec, Class<R> projectionType, Pageable pageable, TypedQuery<T> query) {
+        if (log.isDebugEnabled()) {
+            query.getHints().entrySet().forEach(e -> log.info("apply query hints -> {} : {}", e.getKey(), e.getValue()));
+        }
         Page<T> result = (pageable == null) ? new PageImpl<>(query.getResultList()) : readPage(query, getDomainClass(), pageable, spec);
         return result.map(item -> projectionFactory.createProjection(projectionType, item));
+    }
+
+    private void applyEntityGraphQueryHints(Query query, Map<String, Object> hints) {
+        for (Map.Entry<String, Object> hint : hints.entrySet()) {
+            query.setHint(hint.getKey(), hint.getValue());
+        }
     }
 }
