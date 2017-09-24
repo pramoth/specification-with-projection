087d62e1afc0dd245a80e411b921d11379d48cfa
Pramoth Suwanpech <pramoth@geniustree.co.th>
2017-09-24 23:21:07 +0700

    update spring-data and boot

--- src/test/java/th/co/geniustree/springdata/jpa/SpecificationExecutorProjectionTests.java
+++ src/test/java/th/co/geniustree/springdata/jpa/SpecificationExecutorProjectionTests.java
@@ -8,12 +8,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import th.co.geniustree.springdata.jpa.domain.Document;
 import th.co.geniustree.springdata.jpa.repository.DocumentRepository;
 import th.co.geniustree.springdata.jpa.specification.DocumentSpecs;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,classes = DemoApplication.class)
+@Transactional
 public class SpecificationExecutorProjectionTests {
     @Autowired
     private DocumentRepository documentRepository;
@@ -23,6 +25,7 @@ public class SpecificationExecutorProjectionTests {
         Specifications<Document> where = Specifications.where(DocumentSpecs.idEq(1L));
         Page<DocumentRepository.DocumentWithoutParent> all = documentRepository.findAll(where, DocumentRepository.DocumentWithoutParent.class, null);
         Assertions.assertThat(all).isNotEmpty();
+        System.out.println(all.getContent());
         Assertions.assertThat(all.getContent().get(0).getDocumentType()).isEqualTo("ต้นฉบับ");
     }
 
